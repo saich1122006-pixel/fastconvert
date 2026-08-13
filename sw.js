04c812bc5e50633da8a4fb182228e01a590646b0
saich1122006-pixel.com <saich1122006@gmail.com>
2026-08-13 09:52:11 +0530

    ads ver

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
     "zoneId": 11564244
-};
-self.lary = "";
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
